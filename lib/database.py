a98013ebb16a3f2b9c89df5fe66616348f7b3814
ian <ianokipla@gmail.com>
2024-01-10 10:14:13 +0300

    edited database.py file

--- lib/database.py
+++ lib/database.py
@@ -1,6 +1,6 @@
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from SQLAlchemy import create_engine
+from SQLAlchemy.orm import sessionmaker
 
 engine = create_engine('sqlite:///restauran.db')
 Session = sessionmaker(bind=engine)
